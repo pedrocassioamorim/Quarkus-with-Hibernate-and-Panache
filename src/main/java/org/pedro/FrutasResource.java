c1edcaec4979abea19af21257eed9149a6e4bb37
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-07-06 12:28:03 -0300

    Creating simple POST method to project

--- src/main/java/org/pedro/FrutasResource.java
+++ src/main/java/org/pedro/FrutasResource.java
@@ -1,6 +1,8 @@
 package org.pedro;
 
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -15,5 +17,14 @@ public class FrutasResource {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Fruta> list() {
         return Fruta.listAll();
+    }
+
+
+    @POST @Transactional
+    public void novaFruta(){
+        Fruta fruta = new Fruta();
+        fruta.name = "Elefante";
+        fruta.quantity = 4;
+        fruta.persist();
     }
 }
