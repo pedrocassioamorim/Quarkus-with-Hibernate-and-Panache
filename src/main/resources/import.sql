9f6bc85ba6e403f798b85a9dc88c1d991db61c8b
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-07-06 12:27:26 -0300

    Instantiating objects in PostgreSQL DB

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into Fruta(name, quantity) VALUES ('Manga', 5);
+insert into Fruta(name, quantity) VALUES ( 'Maçã', 3);
+insert into Fruta(name, quantity) VALUES ('Mamão', 6);
+insert into Fruta(name, quantity) VALUES ('Pera', 9);
+insert into Fruta(name, quantity) VALUES ('Uva', 12);
+insert into Fruta(name, quantity) VALUES ('Laranja', 0);
+insert into Fruta(name, quantity) VALUES ('Goiaba', 1);
